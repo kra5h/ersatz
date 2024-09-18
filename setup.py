297d8ad60c0d483c93ce0b92c2af7ce482d1b01d
Evgenii Tsymbalov <krashbest@gmail.com>
2024-09-18 12:01:00 +0200

    Lib bump

--- setup.py
+++ setup.py
@@ -112,8 +112,8 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires = [
         'typing;python_version<"3.5"',
-        'torch==1.7.1',
-        'sentencepiece==0.1.95',
+        'torch==2.4.1',
+        'sentencepiece==0.2.0',
         'tensorboard==2.4.1',
         'progressbar2'
     ],
